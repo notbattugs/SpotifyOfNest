a2888450f347904efec6c9f44a2951a61a442af7
Battugs Bayanbileg <battugs@Battugss-MacBook-Air.local>
2023-05-19 13:35:06 +0800

    asd

---
+++ index.js
@@ -0,0 +1,52 @@
+const array = [
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+  (apple = {
+    name: 'apple',
+    old: 100,
+    health: 102301230,
+  }),
+];
+let map =
+  array &&
+  array.map((el) => {
+    console.log(el.name, el.old, el.health);
+  });
